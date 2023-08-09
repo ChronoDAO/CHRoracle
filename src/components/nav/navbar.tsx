487431f4998a5d526a992939e2df13ee6769e90c
Rea <andrea.solisg@gmail.com>
2023-08-09 23:00:16 +0200

    side-bar in place

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -1,42 +1,81 @@
 import Link from "next/link";
-import navbar from "./navbar.module.scss";
+import styles from "./navbar.module.scss";
+import { MdOutlineSpaceDashboard, MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import { LiaDragonSolid } from "react-icons/lia";
+import { HiUsers } from "react-icons/hi";
+import { GiAxeSword } from "react-icons/gi";
+import { VscGraph } from "react-icons/vsc";
+import { BiSolidCastle } from "react-icons/bi";
+import { useState } from "react";
+
 
 const nav = () => {
+  const [isBarCollapsed, setIsBarCollapsed] = useState(true);
+
+  const toggleSideBarHandler = () => {
+    setIsBarCollapsed((prev) => !prev)
+  }
+
   return (
-    <nav id={navbar.navbar}>
-      <div className={navbar.navContainer}>
-        <Link href="/" className={navbar.links}>
-          <div className="logo">Home!</div>
-        </Link>
-        <ul className={navbar.linksList}>
-          <li>
-            {/* Link to the /dashboard page */}
-            <Link href="/dashboard" className={navbar.links}>
-              {" "}
-              Dashboard
+    <div className={styles["sidebar-wrapper"]} data-collapse={isBarCollapsed} >
+      <button className={styles.btn} onClick={toggleSideBarHandler }>
+        <MdOutlineKeyboardArrowLeft/>
+      </button>
+      <aside className={styles.sidebar} id="navbar">
+        <div className={styles["top-sidebar"]} onClick={toggleSideBarHandler }>
+          <span className={styles.logo}><LiaDragonSolid size={36}/></span>
+          <span className={styles.title}>Chrono Data</span>
+
+        </div>
+
+        <div className={styles["list-container"]}>
+          <ul className={styles["list-items"]}>
+            <Link href="/" className={styles.links}>
+              <li className={styles.item}>
+                <span className={styles["item-icon"]}>
+                  <BiSolidCastle />
+                </span>
+                <span className={styles["item-title"]}>home</span>
+              </li>
             </Link>
-          </li>
-          <li>
-            {/* Link to the /materialtable page */}
-            <Link href="/users" className={navbar.links}>
-              Users{" "}
+
+            <Link href="/dashboard" className={styles.links}>
+              <li className={styles.item}>
+                <span className={styles["item-icon"]}>
+                  <MdOutlineSpaceDashboard />
+                </span>
+                <span className={styles["item-title"]}>Dashboard</span>
+              </li>
             </Link>
-          </li>
-          <li>
-            {/* Link to the /materialtable page */}
-            <Link href="/items" className={navbar.links}>
-              Items
+
+            <Link href="/users" className={styles.links}>
+              <li className={styles.item}>
+                <span className={styles["item-icon"]}>
+                  <HiUsers />
+                </span>
+                <span className={styles["item-title"]}>Users</span>
+              </li>
             </Link>
-          </li>
-          <li>
-            {/* Link to the /materialtable page */}
-            <Link href="/3dgraph" className={navbar.links}>
-              Force Graph
+            <Link href="/items" className={styles.links}>
+              <li className={styles.item}>
+                <span className={styles["item-icon"]}>
+                  <GiAxeSword />
+                </span>
+                <span className={styles["item-title"]}>Items</span>
+              </li>
             </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            <Link href="/3dgraph" className={styles.links}>
+              <li className={styles.item}>
+                <span className={styles["item-icon"]}>
+                  <VscGraph />
+                </span>
+                <span className={styles["item-title"]}>Force Graph</span>
+              </li>
+            </Link>
+          </ul>
+        </div>
+      </aside>
+    </div>
   );
 };
 
