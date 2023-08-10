bed08329dcf4cb843ce01e0b83d3ea69d01bfc26
Lionel Diot <diot.lionel2@gmail.com>
2023-08-10 14:00:42 +0200

    PR pour faire des tests

--- src/pages/users/[username]/index.tsx
+++ src/pages/users/[username]/index.tsx
@@ -8,93 +8,100 @@ import styles from "./userName.module.scss";
 export default function User({
   user,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "archetypeId", //simple recommended way to define a column
-        header: "ID",
-        muiTableHeadCellProps: { sx: { color: "black" } }, //custom props
-        Cell: ({ renderedCellValue }) => (
-          <Link href={`/items/${encodeURIComponent(renderedCellValue)}`}>
-            {renderedCellValue}
-          </Link>
-        ), //optional custom cell render
-      },
-      {
-        accessorKey: "item.name", //simple recommended way to define a column
-        header: "Item Name",
-        muiTableHeadCellProps: { sx: { color: "grey" } }, //custom props
-        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
-      },
-      {
-        accessorKey: "item.floorPrice", //simple recommended way to define a column
-        header: "FloorPrice $",
-        muiTableHeadCellProps: { sx: { color: "red" } }, //custom props
-        Cell: ({ renderedCellValue }) => (
-          <strong>
-            {Number(renderedCellValue.toFixed(2)).toLocaleString()}
-          </strong>
-        ), //optional custom cell render
-      },
-      {
-        accessorKey: "item.rarityName", //simple recommended way to define a column
-        header: "Rarity",
-        muiTableHeadCellProps: { sx: { color: "blue" } }, //custom props
-        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
-      },
-      {
-        accessorKey: "item.maxIssuance", //simple recommended way to define a column
-        header: "Max Issuance",
-        muiTableHeadCellProps: { sx: { color: "green" } }, //custom props
-        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
-      },
-    ],
-    []
-  );
+  try {
+    if (!user) {
+      return <p>User not found.</p>;
+    }
+    const columns = useMemo(
+      () => [
+        {
+          accessorKey: "archetypeId", //simple recommended way to define a column
+          header: "ID",
+          muiTableHeadCellProps: { sx: { color: "black" } }, //custom props
+          Cell: ({ renderedCellValue }) => (
+            <Link href={`/items/${encodeURIComponent(renderedCellValue)}`}>
+              {renderedCellValue}
+            </Link>
+          ), //optional custom cell render
+        },
+        {
+          accessorKey: "item.name", //simple recommended way to define a column
+          header: "Item Name",
+          muiTableHeadCellProps: { sx: { color: "grey" } }, //custom props
+          Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
+        },
+        {
+          accessorKey: "item.floorPrice", //simple recommended way to define a column
+          header: "FloorPrice $",
+          muiTableHeadCellProps: { sx: { color: "red" } }, //custom props
+          Cell: ({ renderedCellValue }) => (
+            <strong>
+              {Number(renderedCellValue.toFixed(2)).toLocaleString()}
+            </strong>
+          ), //optional custom cell render
+        },
+        {
+          accessorKey: "item.rarityName", //simple recommended way to define a column
+          header: "Rarity",
+          muiTableHeadCellProps: { sx: { color: "blue" } }, //custom props
+          Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
+        },
+        {
+          accessorKey: "item.maxIssuance", //simple recommended way to define a column
+          header: "Max Issuance",
+          muiTableHeadCellProps: { sx: { color: "green" } }, //custom props
+          Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
+        },
+      ],
+      []
+    );
 
-  // Calculate the sum of floorPrice values for all NFTs
-  let sumFloorPrice = user.nfts.reduce((total, nft) => {
-    return total + (nft.item?.floorPrice || 0);
-  }, 0);
+    // Calculate the sum of floorPrice values for all NFTs
+    let sumFloorPrice = user.nfts.reduce((total, nft) => {
+      return total + (nft.item?.floorPrice || 0);
+    }, 0);
 
-  sumFloorPrice = Number(sumFloorPrice.toFixed(2));
+    sumFloorPrice = Number(sumFloorPrice.toFixed(2));
 
-  return (
-    <>
-      {user ? (
-        <>
-          <div className={styles.container}>
-            <div className={styles["user-name"]}>
-              <h1>{user.name}</h1>
+    return (
+      <>
+        {user ? (
+          <>
+            <div className={styles.container}>
+              <div className={styles["user-name"]}>
+                <h1>{user.name}</h1>
+              </div>
+
+              <div className={styles["info-container"]}>
+                <h3>
+                  Balance Purchases/Sales:{" "}
+                  {Number(user.balance.toFixed(2)).toLocaleString()} $
+                </h3>
+                <h3>
+                  Sum of Floor Prices of Owned NFTs:{" "}
+                  {sumFloorPrice.toLocaleString()} $
+                </h3>
+                <h3>
+                  Value Generated on BigTime:{" "}
+                  {Number(
+                    (user.balance + sumFloorPrice).toFixed(2)
+                  ).toLocaleString()}{" "}
+                  $
+                </h3>
+              </div>
             </div>
-
-            <div className={styles["info-container"]}>
-
-            <h3>
-              Balance Purchases/Sales:{" "}
-              {Number(user.balance.toFixed(2)).toLocaleString()} $
-            </h3>
-            <h3>
-              Sum of Floor Prices of Owned NFTs:{" "}
-              {sumFloorPrice.toLocaleString()} $
-            </h3>
-            <h3>
-              Value Generated on BigTime:{" "}
-              {Number(
-                (user.balance + sumFloorPrice).toFixed(2)
-              ).toLocaleString()}{" "}
-              $
-            </h3>
-            </div>
-          </div>
-          <Table viewName="NTF's" columns={columns} data={user.nfts} />
-        </>
-      ) : (
-        <p>User not found.</p>
-      )}
-      <Link href="/users">Go back to Users list</Link>
-    </>
-  );
+            <Table viewName="NTF's" columns={columns} data={user.nfts} />
+          </>
+        ) : (
+          <p>User not found.</p>
+        )}
+        <Link href="/users">Go back to Users list</Link>
+      </>
+    );
+  } catch (error) {
+    console.error("Error in User component:", error);
+    return <p>An error occurred. Please try again later.</p>;
+  }
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
