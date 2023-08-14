8fed05c586eae45192812bcd6229cce6010c7922
Rea <andrea.solisg@gmail.com>
2023-08-14 19:17:09 +0200

    distribution of owners to item view

--- src/pages/items/[archetypeid]/index.tsx
+++ src/pages/items/[archetypeid]/index.tsx
@@ -5,73 +5,8 @@ import { useMemo } from "react";
 import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
 import styles from "./archetypeID.module.scss";
 import { getOwnersGroupedByOwners } from "../../../lib/nfts-grouped-by-owners";
-import NFTChart from "../../../components/charts/NFTchart";
+import OwnersPieChart from "../../../components/charts/OwnersPieChart";
 
-
-
-export default function Item({
-  item,
-  totalOwners,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const columns = useMemo(
-    () => [
-      {
-        accessorKey: "issuedId", //simple recommended way to define a column
-        header: "issuedId",
-        muiTableHeadCellProps: { sx: { color: "gray" } }, //custom props
-        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
-      },
-      {
-        accessorKey: "ownerName", //simple recommended way to define a column
-        header: "Owner",
-        muiTableHeadCellProps: { sx: { color: "skyblue" } }, //custom props
-        Cell: ({ renderedCellValue }) => (
-          <Link href={`/users/${encodeURIComponent(renderedCellValue)}`}>
-            {renderedCellValue}
-          </Link>
-        ), //optional custom cell render
-      },
-    ],
-    []
-  );
-
-  return (
-    <>
-      <div className={styles.container}>
-        <div className={styles["item-name"]}>
-          <h1>{item.name}</h1>
-        </div>
-        <div className={styles["body-container"]}>
-          <div className={styles["img-container"]}>
-            <img src={item.imageUrl} className={styles.img} />
-          </div>
-          <div className={styles["info-container"]}>
-            <div className={styles["line-spacing"]}>
-              <h3>Max Issuance : {item.maxIssuance}</h3>
-              <h3>
-                FloorPrice :{" "}
-                {Number(item.floorPrice.toFixed(2)).toLocaleString()}
-              </h3>
-
-              {item.setName ? <h3>{item.setName}</h3> : <h3>Pas de set</h3>}
-              <h3>Number of NFTs issued: {item.nfts.length}</h3>
-            </div>
-          </div>
-          <div className={styles["owners-info-container"]}>
-            <h1> Owners: {totalOwners} </h1>
-            <div className={styles["diagram-container"]}> 
-              <NFTChart nftsCount={item.nfts.length} maxIssuance={item.maxIssuance} />
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      
-      <Table viewName="NFT's" columns={columns} data={item.nfts} />
-      <Link href="/items">Go back to Items list</Link>
-    </>
-  );
-}
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   // @ts-ignore
@@ -104,3 +39,80 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   };
 };
+
+
+export default function Item({
+  item,
+  ownersGrouped,
+  totalOwners,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "issuedId", //simple recommended way to define a column
+        header: "issuedId",
+        muiTableHeadCellProps: { sx: { color: "gray" } }, //custom props
+        Cell: ({ renderedCellValue }) => <strong>{renderedCellValue}</strong>, //optional custom cell render
+      },
+      {
+        accessorKey: "ownerName", //simple recommended way to define a column
+        header: "Owner",
+        muiTableHeadCellProps: { sx: { color: "skyblue" } }, //custom props
+        Cell: ({ renderedCellValue }) => (
+          <Link href={`/users/${encodeURIComponent(renderedCellValue)}`}>
+            {renderedCellValue}
+          </Link>
+        ), //optional custom cell render
+      },
+    ],
+    []
+  );
+  console.log(item)
+  return (
+   
+    <>
+      <div className={styles.container}>
+        <div className={styles["left-side"]}>
+          <div className={styles.top}>
+            <div className={styles["item-name"]}>
+              <h1>{item.name}</h1>
+            </div>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles["img-container"]}>
+              <img src={item.imageUrl} className={styles.img} />
+            </div>
+            <div className={styles["info-container"]}>
+              <div className={styles["line-spacing"]}>
+                <h3>Max Issuance : {item.maxIssuance}</h3>
+                <h3>
+                  FloorPrice :{" "}
+                  {Number(item.floorPrice.toFixed(2)).toLocaleString()}
+                </h3>
+
+                {item.setName ? <h3>{item.setName}</h3> : <h3>Pas de set</h3>}
+                <h3>Number of NFTs issued: {item.nfts.length}</h3>
+                <div className={styles["btn-container"]}>
+                  <Link href={`/items/${item.archetypeId}/owners`}>
+                  <button className={styles["btn-1"]}>
+                    Owners: {totalOwners}
+                  </button>
+                  </Link>
+                
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles["right-side"]}>
+          <div className={styles["diagram-container"]}>
+            <OwnersPieChart rawData={ownersGrouped} total={item.nfts.length} />
+          </div>
+        </div>
+      </div>
+
+      <Table viewName="NFT's" columns={columns} data={item.nfts} />
+      <Link href="/items">Go back to Items list</Link>
+    </>
+  );
+}
