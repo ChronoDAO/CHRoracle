b72598b5fafbd008f9df1a25a68d6f7e89e97046
Rea <andrea.solisg@gmail.com>
2023-08-17 13:35:24 +0200

    market value by floor price

---
+++ src/lib/market-value-by-floor-price.ts
@@ -0,0 +1,21 @@
+import prisma from "./prisma";
+
+export const marketValueByFloorPrice = async () => {
+    const items = await prisma.item.findMany({
+        include: { 
+          nfts: true,
+      }});
+      
+    let sumNftsFloorPrice = 0
+      let totalNFTs = 0
+    items.forEach((item) =>{
+      totalNFTs += item.nfts.length
+      if (item.floorPrice){
+          sumNftsFloorPrice += item.floorPrice * item.nfts.length
+      }  
+    })
+
+  return {
+    sumNftsFloorPrice: sumNftsFloorPrice,totalNFTs: totalNFTs
+  };
+};
