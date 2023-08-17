0266346df1996153f882a418edf059ae3780db41
Rea <andrea.solisg@gmail.com>
2023-08-17 13:34:55 +0200

    fomated

--- src/lib/last-sales.ts
+++ src/lib/last-sales.ts
@@ -1,24 +1,24 @@
-import prisma from './prisma'
+import prisma from "./prisma";
 
-export const lastSales = async() => {
-    const lastSales = await prisma.sale.findMany({
-        take: 5,
-        orderBy: {
-          id: 'desc',
-        },
+export const lastSales = async () => {
+  const lastSales = await prisma.sale.findMany({
+    take: 5,
+    orderBy: {
+      id: "desc",
+    },
+    include: {
+      nft: {
         include: {
-          nft: {
-              include: {
-                  item: {
-                      select:{
-                          name: true,
-                          imageUrl: true,
-                          rarityName: true,
-                      }
-                  }
-              }
-          }
-        }
-      })
-      return lastSales
-}
+          item: {
+            select: {
+              name: true,
+              imageUrl: true,
+              rarityName: true,
+            },
+          },
+        },
+      },
+    },
+  });
+  return lastSales;
+};
