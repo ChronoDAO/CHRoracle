37f3d1955e5a10aeefea815a3fb7c1952ac59be2
Rea <andrea.solisg@gmail.com>
2023-08-21 12:45:41 +0200

    icons removed

--- components/nav/navbar.tsx
+++ components/nav/navbar.tsx
@@ -1,34 +1,34 @@
-'use client'
+"use client";
 import Link from "next/link";
 import styles from "./navbar.module.scss";
-import { MdOutlineSpaceDashboard, MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import {
+  MdOutlineSpaceDashboard,
+  MdOutlineKeyboardArrowLeft,
+} from "react-icons/md";
 import { LiaDragonSolid } from "react-icons/lia";
 import { HiUsers } from "react-icons/hi";
 import { GiAxeSword } from "react-icons/gi";
-import { VscGraph } from "react-icons/vsc";
 import { BiSolidCastle } from "react-icons/bi";
-import { RiNftFill } from "react-icons/ri";
-
 import { useState } from "react";
-
 
 const nav = () => {
   const [isBarCollapsed, setIsBarCollapsed] = useState(true);
 
   const toggleSideBarHandler = () => {
-    setIsBarCollapsed((prev) => !prev)
-  }
+    setIsBarCollapsed((prev) => !prev);
+  };
 
   return (
-    <div className={styles["sidebar-wrapper"]} data-collapse={isBarCollapsed} >
-      <button className={styles.btn} onClick={toggleSideBarHandler }>
-        <MdOutlineKeyboardArrowLeft/>
+    <div className={styles["sidebar-wrapper"]} data-collapse={isBarCollapsed}>
+      <button className={styles.btn} onClick={toggleSideBarHandler}>
+        <MdOutlineKeyboardArrowLeft />
       </button>
       <aside className={styles.sidebar} id="navbar">
-        <div className={styles["top-sidebar"]} onClick={toggleSideBarHandler }>
-          <span className={styles.logo}><LiaDragonSolid size={36}/></span>
+        <div className={styles["top-sidebar"]} onClick={toggleSideBarHandler}>
+          <span className={styles.logo}>
+            <LiaDragonSolid size={36} />
+          </span>
           <span className={styles.title}>Chrono Data</span>
-
         </div>
 
         <div className={styles["list-container"]}>
@@ -65,23 +65,6 @@ const nav = () => {
                   <GiAxeSword />
                 </span>
                 <span className={styles["item-title"]}>Items</span>
-              </li>
-            </Link>
-            <Link href="/3dgraph" className={styles.links}>
-              <li className={styles.item}>
-                <span className={styles["item-icon"]}>
-                  <VscGraph />
-                </span>
-                <span className={styles["item-title"]}>Graph</span>
-              </li>
-            </Link>
-
-            <Link href="/nft" className={styles.links}>
-            <li className={styles.item}>
-                <span className={styles["item-icon"]}>
-                  <RiNftFill/>
-                </span>
-                <span className={styles["item-title"]}>NFT</span>
               </li>
             </Link>
           </ul>
