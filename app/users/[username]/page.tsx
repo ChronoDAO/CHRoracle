2d9154263eba7f6051595856c62ec4bad5b02cea
Lionel Diot <diot.lionel2@gmail.com>
2023-08-21 17:21:57 +0200

    more commenting

--- app/users/[username]/page.tsx
+++ app/users/[username]/page.tsx
@@ -27,35 +27,35 @@ export default async function User({ params: { username } }: Params) {
   if (!user) {
     return <UsernameNotFound username={username} />;
   }
-   // Convertir les dates en chaînes ISO et gérer les valeurs null
-   const purchasesWithSerializedDates = user.purchases.map((purchase) => ({
-    ...purchase,
-    date: purchase.date.toISOString(),
-  }));
+  //  // Convertir les dates en chaînes ISO et gérer les valeurs null
+  //  const purchasesWithSerializedDates = user.purchases.map((purchase) => ({
+  //   ...purchase,
+  //   date: purchase.date.toISOString(),
+  // }));
 
-  const dropsWithSerializedDates = user.drops.map((drop) => ({
-    ...drop,
-    date: drop.date.toISOString(),
-  }));
+  // const dropsWithSerializedDates = user.drops.map((drop) => ({
+  //   ...drop,
+  //   date: drop.date.toISOString(),
+  // }));
 
-  const nftsWithSerializedDates = user.nfts.map((nft) => ({
-    ...nft,
-    lootDate: nft.lootDate ? new Date(nft.lootDate).toISOString() : '',
-  }));
+  // const nftsWithSerializedDates = user.nfts.map((nft) => ({
+  //   ...nft,
+  //   lootDate: nft.lootDate ? new Date(nft.lootDate).toISOString() : '',
+  // }));
 
-  const archetypeIds = nftsWithSerializedDates.map((nft) => nft.archetypeId).filter((archetypeId) => archetypeId !== null) as string[];
+  // const archetypeIds = nftsWithSerializedDates.map((nft) => nft.archetypeId).filter((archetypeId) => archetypeId !== null) as string[];
 
-  const uniqueArchetypeIds = [...new Set(archetypeIds)];
-  const uniqueNFTCount = uniqueArchetypeIds.length;
-  let data = {
-    user: {
-      ...user,
-      purchases: purchasesWithSerializedDates,
-      drops: dropsWithSerializedDates,
-      nfts: nftsWithSerializedDates,
-    },
-    uniqueNFTCount,
-  };
+  // const uniqueArchetypeIds = [...new Set(archetypeIds)];
+  // const uniqueNFTCount = uniqueArchetypeIds.length;
+  // let data = {
+  //   user: {
+  //     ...user,
+  //     purchases: purchasesWithSerializedDates,
+  //     drops: dropsWithSerializedDates,
+  //     nfts: nftsWithSerializedDates,
+  //   },
+  //   uniqueNFTCount,
+  // };
   //@ts-ignore
   return (
   // <GenerateUserTable data={data} />
