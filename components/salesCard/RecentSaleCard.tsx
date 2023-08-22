6fbb4d6ff8e095549af2cb9eb1d123b2d8ddb3b2
Rea <andrea.solisg@gmail.com>
2023-08-22 16:36:36 +0200

    handle date function

--- components/salesCard/RecentSaleCard.tsx
+++ components/salesCard/RecentSaleCard.tsx
@@ -58,6 +58,24 @@ interface Item {
   nfts: NFT[];
 }
 
+const getTimeDifference = (date: Date): string => {
+  const now = new Date();
+  const diffInSeconds = Math.floor((now.getTime() - date.getTime()) / 1000);
+
+  if (diffInSeconds < 60) {
+    return "a few seconds ago";
+  } else if (diffInSeconds < 3600) {
+    const minutes = Math.floor(diffInSeconds / 60);
+    return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+  } else if (diffInSeconds < 86400) {
+    const hours = Math.floor(diffInSeconds / 3600);
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
+  } else {
+    const days = Math.floor(diffInSeconds / 86400);
+    return `${days} ${days === 1 ? "day" : "days"} ago`;
+  }
+};
+
 const RecentSaleCard: React.FC<TypeItemSales> = ({
   price,
   date,
@@ -67,6 +85,7 @@ const RecentSaleCard: React.FC<TypeItemSales> = ({
   itemName,
   imageUrl,
 }) => {
+  const timeDifference = getTimeDifference(new Date(date));
   return (
     <div className={styles.nft}>
       <div className={styles.main}>
@@ -96,8 +115,7 @@ const RecentSaleCard: React.FC<TypeItemSales> = ({
           </div>
           <div className={styles.date}>
             <ins>◷</ins>
-            <p>..few moments ago</p>
-            {/* <p>{new Date(date).toLocaleString()}</p> */}
+            {timeDifference}
           </div>
         </div>
       </div>
