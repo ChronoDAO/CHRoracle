2a80eb3d59598135037aa18f73059577b5e81bf0
Rea <andrea.solisg@gmail.com>
2023-08-22 09:31:15 +0200

    sales by day in dashbord

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,18 @@
-
+import {getSales} from '@/lib/recent-sales';
+import {getSalesByDay} from '@/lib/sales-by-day';
 import RecentSales from '../../components/RecentSales/RecentSales';
-import prisma from "../../lib/prisma";
-async function getSales() {
-  let recentSales = await prisma.sale.findMany({
-    take: 5, // Limit the result to 5 records
-    orderBy: {
-      date: 'desc', // Order by createdAt field in descending order (most recent first)
-    },
-    include: {
-      nft: {
-        include: {
-          item: true,
-        },
-      },
-    },
-  });
-  return recentSales;
-}
+import SalesByDay from '@/components/salesByDay/salesByDay';
+
 export default async function Dashboard() {
   let recentSales = await getSales();
-
+  let salesByDay = await getSalesByDay()
 
   return (
     <>
     <h1>Bienvenue sur Dashboard</h1>
     {/* @ts-ignore */}
       <RecentSales data={ recentSales }  />
+      <SalesByDay data= {salesByDay}/>
     </>
   )
 
