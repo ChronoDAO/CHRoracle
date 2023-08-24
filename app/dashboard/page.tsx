b99f3315ca4f03a5e1863c50f5d24e9f748c56df
Rea <andrea.solisg@gmail.com>
2023-08-24 11:32:08 +0200

    sales conditional rendering for all, 30d and 7d

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,8 @@ import { getSales } from "@/lib/recent-sales";
 import { getSalesByDay } from "@/lib/sales-by-day";
 import { getLast24hSales } from "@/lib/sales-last-24h";
 import RecentSales from "@/components/RecentSales/RecentSales";
-import SalesByDay from "@/components/salesByDay/salesByDay";
+import Sales from "@/components/Sales/Sales";
+
 
 
 export default async function Dashboard() {
@@ -11,10 +12,14 @@ export default async function Dashboard() {
   let last24hsales = await getLast24hSales("2023-06-25T02:00:00.000Z");
 
   return (
+   
     <>
       {/* @ts-ignore */}
       <RecentSales data={recentSales} />
-      <SalesByDay data={salesByDay} data24h={last24hsales} />
+
+      <Sales data={salesByDay} data24h={last24hsales}/>
+
+      
     </>
   );
 }
