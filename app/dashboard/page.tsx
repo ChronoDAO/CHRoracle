821c437ca681fbcb6154b6d792d8b73d33be464b
Rea <andrea.solisg@gmail.com>
2023-08-28 15:05:11 +0200

    css adjustments

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,12 @@
-import { getSales } from "@/lib/recent-sales";
 import { getSalesByDay } from "@/lib/sales-by-day";
 import { getLast24hSales } from "@/lib/sales-last-24h";
-import RecentSales from "@/components/RecentSales/RecentSales";
 import Sales from "@/components/Sales/Sales";
 import {getNftItem } from "@/lib/sales-with-nft-info"
 import LatestSalesByCategory from '@/components/LatestSalesByCategory/LatestSalesByCategory'
 import { getCategories } from "@/lib/categories";
 
-
-
 export default async function Dashboard() {
-  let recentSales = await getSales();
+
   let salesByDay = await getSalesByDay();
   let last24hsales = await getLast24hSales("2023-06-25T02:00:00.000Z");
   let sales= await getNftItem()
@@ -21,7 +17,7 @@ export default async function Dashboard() {
    
     <>
       {/* @ts-ignore */}
-      <RecentSales data={recentSales} />
+ 
 
       <LatestSalesByCategory data={sales} categories = {categories}/>
 
