c2d5d67ca4cb5d2f6a4674f9cf19a25ca5ec4ef8
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-29 16:49:44 +0200

    LD hourglass loading

--- app/testroutes/page.tsx
+++ app/testroutes/page.tsx
@@ -3,197 +3,21 @@ import React from 'react'
 
 export default function Loading () {
   return(
-    <>
-		<div className={styles["socket"]}>
-			<div className={styles["gel center-gel"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
+    <div className={styles["loading"]}>
+			<div className={styles["hourglassBackground"]}>
+				<div className={styles["hourglassContainer"]}>
+					<div className={styles["hourglassCurves"]}></div>
+					<div className={styles["hourglassCapTop"]}></div>
+					<div className={styles["hourglassGlassTop"]}></div>
+					<div className={styles["hourglassSand"]}></div>
+					<div className={styles["hourglassSandStream"]}></div>
+					<div className={styles["hourglassCapBottom"]}></div>
+					<div className={styles["hourglassGlass"]}></div>
+				</div>
 			</div>
-      <div className={styles["gel c1 r1"]}>
-			  <div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
+			<div className={styles["text-container"]}>
+				<p>Chargement en cours</p>
 			</div>
-			<div className={styles["gel c2 r1"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c3 r1"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c4 r1"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c5 r1"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c6 r1"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-
-			<div className={styles["gel c7 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c8 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c9 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c10 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c11 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c12 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c13 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c14 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c15 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c16 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c17 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c18 r2"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-
-			<div className={styles["gel c19 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c20 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c21 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c22 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c23 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c24 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c25 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c26 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c28 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c29 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c30 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c31 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c32 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c33 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c34 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c35 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c36 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-			<div className={styles["gel c37 r3"]}>
-				<div className={styles["hex-brick h1"]}></div>
-				<div className={styles["hex-brick h2"]}></div>
-				<div className={styles["hex-brick h3"]}></div>
-			</div>
-
-		</div>
-    </>
+    </div>
   )
 }
