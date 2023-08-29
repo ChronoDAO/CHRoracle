7d9f63891157d2b1d3fbc233bd75070c9e734844
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-29 17:41:24 +0200

    LD creating the loading components (and returning test-routes to it's original state

--- app/testroutes/page.tsx
+++ app/testroutes/page.tsx
@@ -1,21 +1,8 @@
-import styles from './loading.module.scss'
-import React from 'react'
 
-export default function Loading () {
-  return(
-    <div className={styles["loading"]}>
-			<div className={styles["pyramid-loader"]}>
-				<div className={styles["wrapper"]}>
-					<span className={styles["side side1"]}></span>
-					<span className={styles["side side2"]}></span>
-					<span className={styles["side side3"]}></span>
-					<span className={styles["side side4"]}></span>
-					<span className={styles["shadow"]}></span>
-				</div>
-			</div>
-			<div className={styles["text-container"]}>
-				<p>Chargement en cours</p>
-			</div>
-    </div>
+export default async function TestRoute() {
+  return (
+    <>
+      <h1>Test route </h1>
+    </>
   )
 }
