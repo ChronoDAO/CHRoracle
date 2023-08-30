60c102f902f41364f7285792f14076106da04580
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-30 15:54:13 +0200

    LD adding the auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,15 +1,14 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
+
 import DiscordProvider from "next-auth/providers/discord"
 
-// export const authOptions = {
-//   // Configure one or more authentication providers
-//   providers: [
-//     DiscordProvider({
-//       // clientId: process.env.GITHUB_ID,
-//       // clientSecret: process.env.GITHUB_SECRET,
-//     }),
-//     // ...add more providers here
-//   ],
-// }
+export const authConfig: NextAuthOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    }),
+  ],
+}
 
-// export default NextAuth(authOptions)
+export default NextAuth(authConfig)
