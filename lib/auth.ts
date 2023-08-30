c61cc28beb74487bd059a68662643a4dec2f20a6
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-08-30 11:10:45 +0200

    LD creating the first files

---
+++ lib/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth"
+import DiscordProvider from "next-auth/providers/discord"
+
+// export const authOptions = {
+//   // Configure one or more authentication providers
+//   providers: [
+//     DiscordProvider({
+//       // clientId: process.env.GITHUB_ID,
+//       // clientSecret: process.env.GITHUB_SECRET,
+//     }),
+//     // ...add more providers here
+//   ],
+// }
+
+// export default NextAuth(authOptions)
