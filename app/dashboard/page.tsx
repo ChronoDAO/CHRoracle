95cbb6b3187b418e25371a40ca82e13e6204290d
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-01 10:53:53 +0200

    LD exchanging the home and the dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,24 +1,9 @@
-import { getSalesByDay } from "@/lib/prisma/sales-by-day";
-import { getLast24hSales } from "@/lib/sales-last-24h";
-import Sales from "@/components/Sales/Sales";
-import {getSalesByCategory } from "@/lib/prisma/sales-with-nft-info"
-import RecentSalesByCategory from '@/components/RecentSalesByCategory/RecentSalesByCategory'
-import { getCategories } from "@/lib/prisma/categories";
+import styles from './dashboard.module.scss'
 
-export default async function Dashboard() {
-
-  let salesByDay = await getSalesByDay();
-  let last24hsales = await getLast24hSales("2023-06-25T02:00:00.000Z");
-  let transformedSales = await getSalesByCategory()
-  let categories = await getCategories()
-
-
+export default function Dashboard() {
   return (
-   
-    <>
-      {/* @ts-ignore */}
-      <RecentSalesByCategory data={transformedSales} categories = {categories}/>
-      <Sales data={salesByDay} data24h={last24hsales}/>   
-    </>
-  );
+    <div className={styles["center-title"]}>
+      <h1>Work in Progress </h1>
+    </div>
+  )
 }
