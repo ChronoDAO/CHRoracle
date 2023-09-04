2e47d4b8941d542a169ff404bb94da298895b845
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-04 16:56:37 +0200

    LD removing the bugged connect funtionnality

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,6 @@
-import { authConfig } from '@/lib/auth'
-import NextAuth from 'next-auth/next'
+// import { authConfig } from '@/lib/auth'
+// import NextAuth from 'next-auth/next'
 
-const handler = NextAuth(authConfig)
+// const handler = NextAuth(authConfig)
 
-export { handler as GET, handler as POST}
+// export { handler as GET, handler as POST}
