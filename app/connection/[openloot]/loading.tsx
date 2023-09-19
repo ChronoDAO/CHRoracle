aca11a2b6a63e8db81db5beb80457a382070a9a7
Cyber-Geooorge <george.cambodge.gmail.com>
2023-09-19 15:03:29 +0200

    script créer

---
+++ app/connection/[openloot]/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingComponent from "@/components/loading/loading";
+
+export default function Loading() {
+  const connection = " load connection page...";
+
+  return <LoadingComponent page={connection} />;
+}
---
+++ app/connection/loading.tsx
@@ -0,0 +1,7 @@
+import LoadingComponent from "@/components/loading/loading";
+
+export default function Loading() {
+  const connection = "Connection";
+
+  return <LoadingComponent page={connection} />;
+}
