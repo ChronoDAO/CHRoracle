3c61633919ea542d5cfbfaf9f73f89aa453d6bbc
Cyber-Geooorge <george.cambodge.gmail.com>
2023-09-21 11:18:19 +0200

    wrapping provider in layout

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,19 +5,18 @@ import { init } from "@socialgouv/matomo-next";
 const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL || "";
 const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID || "";
 
-interface MyProps {
-  Component: React.ElementType;
-  pageProps: any;
-}
+type MyProps = {
+  children?: React.ReactNode;
+};
 
-function MyApp({ Component, pageProps }: MyProps) {
+function MyAppProvider({ children }: MyProps) {
   useEffect(() => {
     if (MATOMO_URL && MATOMO_SITE_ID) {
       init({ url: MATOMO_URL, siteId: MATOMO_SITE_ID });
     }
   }, []);
 
-  return <Component {...pageProps} />;
+  return <>{children}</>;
 }
 
-export default MyApp;
+export default MyAppProvider;
