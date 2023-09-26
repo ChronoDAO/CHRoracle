cbcabaa9e00f8047e799bf5bbbe5409f608f166b
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-26 10:40:22 +0200

    LD FEAT adding a 404 page with tailwind

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,9 +3,13 @@
 
 const NotFound = () => {
   return (
-    <p>
-      The page you're looking for cannot be found !
-    </p>
+    <section className="min-h-screen pt-20">
+      <div className="max-w-3xl mx-auto bg-ct-dark-100rounded-md  h-[20rem] flex justify-center items-center">
+        <p className="text-3xl font-semibold">
+          The page you're looking for cannot be found !
+        </p>
+      </div>
+    </section>
   )
 };
 
