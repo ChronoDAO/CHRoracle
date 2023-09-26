bb5870a9366b45eb48e925aae93032b2933a6446
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-26 14:31:17 +0200

    LD STYLES removing the image for now and changing the min-h-screen by full

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,8 +3,8 @@
 
 const NotFound = () => {
   return (
-    <section className="min-h-screen pt-20">
-      <div className="max-w-3xl mx-auto rounded-md bg-not-found h-[20rem] flex justify-center items-center overflow-hidden">
+    <section className="min-h-full pt-20 bg-not-found">
+      <div className="max-w-3xl mx-auto rounded-md h-[20rem] flex justify-center items-center overflow-hidden">
         <p className="text-3xl font-semibold">
           The page you're looking for cannot be found !
         </p>
