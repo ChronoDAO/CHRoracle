8442fd08d8093fd6ac0ad160aab34cd4b31d45dd
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-09-26 12:23:53 +0200

    LD STYLE adding a overflow hidden to the 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@
 const NotFound = () => {
   return (
     <section className="min-h-screen pt-20">
-      <div className="max-w-3xl mx-auto rounded-md bg-not-found h-[20rem] flex justify-center items-center">
+      <div className="max-w-3xl mx-auto rounded-md bg-not-found h-[20rem] flex justify-center items-center overflow-hidden">
         <p className="text-3xl font-semibold">
           The page you're looking for cannot be found !
         </p>
